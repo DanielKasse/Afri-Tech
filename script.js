8f5ae721e8fe6c8677eb52c658bcb79e3e109c99
Daniel <danielyetwale02@gmail.com>
2023-04-08 06:30:57 +0300

    adding search bar and some home pages

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+document.querySelector('#seach-icon').onclick = ()=>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true;
+});
